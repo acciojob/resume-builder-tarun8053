8de5bf0672fdeab597acd89edf230f9c05dd0bd4
Tarun <thakurtarun1012@gmail.com>
2026-01-11 22:55:19 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,59 +5,26 @@ export default function Education() {
   const dispatch = useDispatch();
   const [edu, setEdu] = useState({});
 
+  const addEdu = () => {
+    if (
+      edu.courseName &&
+      edu.completionYear &&
+      edu.college &&
+      edu.percentage
+    ) {
+      dispatch({ type: "ADD_EDU", payload: edu });
+    }
+  };
+
   return (
     <>
-      <input
-        type="text"
-        name="courseName"
-        placeholder="Course Name"
-        onChange={(e) =>
-          setEdu({ ...edu, courseName: e.target.value })
-        }
-      />
+      <input name="courseName" onChange={e=>setEdu({...edu,courseName:e.target.value})}/>
+      <input name="completionYear" onChange={e=>setEdu({...edu,completionYear:e.target.value})}/>
+      <input name="college" onChange={e=>setEdu({...edu,college:e.target.value})}/>
+      <input name="percentage" onChange={e=>setEdu({...edu,percentage:e.target.value})}/>
 
-      <input
-        type="number"
-        name="completionYear"
-        placeholder="Completion Year"
-        onChange={(e) =>
-          setEdu({ ...edu, completionYear: e.target.value })
-        }
-      />
-
-      <input
-        type="text"
-        name="college"
-        placeholder="College Name"
-        onChange={(e) =>
-          setEdu({ ...edu, college: e.target.value })
-        }
-      />
-
-      <input
-        type="number"
-        name="percentage"
-        placeholder="Percentage"
-        onChange={(e) =>
-          setEdu({ ...edu, percentage: e.target.value })
-        }
-      />
-
-      <button
-        id="add_education"
-        onClick={() =>
-          dispatch({ type: "ADD_EDU", payload: edu })
-        }
-      >
-        Add
-      </button>
-
-      <button
-        id="delete"
-        onClick={() => dispatch({ type: "DEL_EDU" })}
-      >
-        Delete
-      </button>
+      <button id="add_education" onClick={addEdu}>Add</button>
+      <button id="delete" onClick={()=>dispatch({type:"DEL_EDU"})}>Delete</button>
     </>
   );
 }
