91f47427c9d9198e7e51d68391b7f5356dc434dd
Tarun <thakurtarun1012@gmail.com>
2026-01-14 15:14:05 +0530

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,15 +3,21 @@ import { useDispatch } from "react-redux";
 
 export default function Social() {
   const dispatch = useDispatch();
-  const [social, setSocial] = useState("");
+  const [social, setSocial] = useState("1");
 
   return (
     <>
-      <input name="Social" onChange={e=>setSocial(e.target.value)} />
+      <h2>Add your Social Media</h2>
+
+      <input
+        name="Social"
+        value={social}
+        onChange={(e) => setSocial(e.target.value)}
+      />
 
       <button
         id="add_social"
-        onClick={() => social && dispatch({ type: "ADD_SOCIAL", payload: social })}
+        onClick={() => dispatch({ type: "ADD_SOCIAL", payload: social })}
       >
         Add
       </button>
